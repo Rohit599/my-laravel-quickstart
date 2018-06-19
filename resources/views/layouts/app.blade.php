b33655d116d1397b414895eb31ec2ab381747017
Rohit <rjakhmola5@gmail.com>
2018-06-19 12:52:27 +0530

    Add Task and layouts Added

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<title>Laravel Quickstart - Basic</title>
+</head>
+<body>
+	<div class="container">
+		<nav class="navbar navbar-default">
+			<!--Navbar contents -->
+		</nav>
+	</div>
+	@yield('content')
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+use Illuminate\Http\Request;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/**
+ * Display All Tasks
+ */
+Route::get('/', function () {
+    $tasks = \App\Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+/**
+ * Add a New Task
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new \App\Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+/**
+ * Delete An Existing Task
+ */
+Route::delete('/task/{id}', function($id) {
+	Route::delete('/task/{id}', function ($id) {
+    Task::findOrFail($id)->delete();
+
+    return redirect('/');
+	});
+});
+
